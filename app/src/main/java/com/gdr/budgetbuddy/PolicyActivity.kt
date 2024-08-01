83a2fe21b16e121db193ddd2cce51718b9e666d0
snowflo <g175151@gmail.com>
2024-08-01 23:13:20 +0900

    policyScreenUI
    temp

---
+++ app/src/main/java/com/gdr/budgetbuddy/PolicyActivity.kt
@@ -0,0 +1,11 @@
+package com.gdr.budgetbuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PolicyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_policy)
+    }
+}
